816e50236d4a7912820c69ea40ed0094a89cdbe3
Clément Hervé <clement.herve@etu.minesparis.psl.eu>
2024-02-02 09:51:58 +0100

    Implémentation de l'algo knn

---
+++ knn.py
@@ -0,0 +1,17 @@
+
+def dist(a,b):
+    return ((a[0]-b[0])**2 + (a[1]-b[1])**2 )**0.5
+
+
+def knn(a,L):
+    xa, ya = a
+    b_min = L[0]
+    dist_min = dist(a, L[0])
+    for x in L[1:]:
+        dist_temp = dist(a, x)
+        if dist_temp < dist_min:
+            dist_min = dist_temp
+            b_min = x
+    return b_min
+
+
