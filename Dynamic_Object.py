417b25d68ac2189d531747d15ecdf921973d9016
Louis Brusset <louis.brusset@etu.minesparis.psl.eu>
2024-02-02 09:55:23 +0100

    Création des fichiers Dynamic_Object et Static_Object

---
+++ Dynamic_Object.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import matplotlib.pyplot as pyplot
+
+
+class DynamicObject :
+    
---
+++ Static_Object.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import matplotlib.pyplot as pyplot
+import pygame
+
+import "knn.py"
+
+
+
+class StaticObject :
+
